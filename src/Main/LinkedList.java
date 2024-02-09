aa0b60f933f9d10e0dd326153fa486542a6dc096
adham khaled <adhammkhaledd.99@gmail.com>
2024-02-10 01:14:07 +0200

    update LinkedList

--- src/Main/LinkedList.java
+++ src/Main/LinkedList.java
@@ -4,9 +4,6 @@ class nodeLinkedList {
     int item;
     nodeLinkedList next;
 
-    nodeLinkedList (int element) {
-        this.item = element;
-    }
 }
 
 public class LinkedList {
@@ -18,9 +15,12 @@ public class LinkedList {
         this.first = this.last = null;
         this.length = 0;
     }
-
+    boolean isEmpty() {
+        return length == 0;
+    }
     void insertAtFirst(int element) {
-        nodeLinkedList newNode = new nodeLinkedList(element);
+        nodeLinkedList newNode = new nodeLinkedList();
+        newNode.item = element;
         if(length == 0) {                   //in this case we add the first node
             last = first = newNode;
             newNode.next = null;
@@ -32,7 +32,8 @@ public class LinkedList {
         length++;
     }
     void insertAtEnd(int element) {
-        nodeLinkedList newNode = new nodeLinkedList(element);
+        nodeLinkedList newNode = new nodeLinkedList();
+        newNode.item = element;
         if(length == 0) {                   //in this case we add the first node
             last = first = newNode;
             newNode.next = null;
@@ -45,7 +46,8 @@ public class LinkedList {
         length++;
     }
     void insertAtPos(int position, int element) {
-        nodeLinkedList newNode = new nodeLinkedList(element);
+        nodeLinkedList newNode = new nodeLinkedList();
+        newNode.item = element;
         nodeLinkedList cur = first;
         for (int i = 1; i < position; i++) {
             cur = cur.next;
@@ -53,6 +55,86 @@ public class LinkedList {
         newNode.next = cur.next;
         cur.next = newNode;
         length++;
+    }
+    void removeFirst() {
+        if(isEmpty()) {
+            System.out.println("LinkedList is Empty can not remove..");
+            return;
+        }
+        else if(length == 1) {
+            first.next = null;
+            last.next = null;
+        }
+        else {
+            first = first.next;
+        }
+        length--;
+    }
+    void removeLast() {
+        if(isEmpty()) {
+            System.out.println("LinkedList is Empty can not remove..");
+            return;
+        }
+        else if(length == 1) {
+            first.next = null;
+            last.next = null;
+        }
+        else {
+            nodeLinkedList cur = first.next;
+            nodeLinkedList prev = first;
+            while(cur != last) {
+                prev = cur;
+                cur = cur.next;
+            }
+            prev.next = null;
+            last = prev;
+        }
+        length--;
+    }
+    void remove(int element) {
+        if(isEmpty()) {
+            System.out.println("LinkedList is Empty can not remove..");
+        }
+        else if(first.item == element) {           //in this case element in first node
+            first = first.next;
+            length--;
+        }
+        else{
+            nodeLinkedList cur = first.next;
+            nodeLinkedList prev = first;
+            while(cur != null) {
+                if(cur.item == element)
+                    break;
+                prev = cur;
+                cur = cur.next;
+            }
+            if(cur == null)
+                System.out.println("element not found");
+            else {
+                prev.next = cur.next;
+                length--;
+            }
+        }
+    }
+    void removeAtPos(int position) {
+        if(isEmpty()) {
+            System.out.println("LinkedList is Empty can not remove..");
+            return;
+        }
+        else if(length == 1) {
+            first.next = null;
+            last.next = null;
+        }
+        else {
+            nodeLinkedList cur = first.next;
+            nodeLinkedList prev = first;
+            for (int i = 0; i < position-1; i++) {
+                prev = cur;
+                cur = cur.next;
+            }
+            prev.next = cur.next;
+        }
+        length--;
     }
     void print() {
         nodeLinkedList cur = first;
