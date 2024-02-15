56e5c19ea8c1821f5eb33c072ac29ce112cf0645
adham khaled <adhammkhaledd.99@gmail.com>
2024-02-15 17:05:36 +0200

    update in Insertion Sort

--- src/Main/InsertionSort.java
+++ src/Main/InsertionSort.java
@@ -7,11 +7,15 @@ public class InsertionSort {
         for (int i = 0; i < size; i++) {  // 80 90 60 30 50 70 40
             key = arr[i];
             j = i - 1;
+            // Compare key with each element on the left of it until an element smaller than
+            // it is found.
+            // For descending order, change key<array[j] to key>array[j].
             while (j >= 0 && arr[j] > key)
             {
                 arr[j + 1] = arr[j];     // shift elements
                 j--;
             }
+            // Place key at after the element just smaller than it.
             arr[j + 1] = key;
         }
     }
