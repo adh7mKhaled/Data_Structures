b2982ae63ef16bdf2e3fe6410c966dee9f50d548
adham khaled <adhammkhaledd.99@gmail.com>
2024-03-10 13:56:58 +0200

    update Binary Search algorithm

--- src/Main/BinarySearch.java
+++ src/Main/BinarySearch.java
@@ -5,7 +5,7 @@ public class BinarySearch {
         while(low  <= high) {
             int mid = (low  + high) / 2; // middle index
             if (arr[mid] == element)
-                return mid;   // return index of key
+                return mid;   // return index of element
             else {
                 if (arr[mid] < element)
                     low  = mid + 1;
@@ -13,6 +13,6 @@ public class BinarySearch {
                     high = mid - 1;
             }
         }
-        return -1;   // if key not found return -1;
+        return -1;   // if element not found return -1;
     }
 }
